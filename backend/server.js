e09080d1d0926423a03f280004a242063d1d53f0
augusto20052008 <115821107+augusto20052008@users.noreply.github.com>
2025-11-23 03:08:09 -0500

    cambio puerto 5000

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 // --- Configuración del Servidor ---
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
